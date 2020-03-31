d9cb837cee2bea5301e6ec18e24d566f45b0ccb5
sofia tofigh <sst2140@columbia.edu>
2020-03-31 02:10:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
-def square_array(array)
-  squared = []
-count = 0
-  while count < array.size do
-    squared << (array[count] ** 2)
-    count = count + 1
+def output_array_elements(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
   end
-  squared
 end
