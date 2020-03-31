6a50623e6b6eb6ef74bca40c96330d22488ad3b4
sofia tofigh <sst2140@columbia.edu>
2020-03-31 02:11:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def output_array_elements(array)
   counter = 0
  
-  while array[counter] do
-    puts array[counter] ** 2
+  while counter < array.size do
+    array << array[counter] ** 2
     counter += 1
   end
 end
